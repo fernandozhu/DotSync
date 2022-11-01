00fdf6f395926345d42e624757003e2481309ac7
Fernando Zhu <fernandoisadev@gmail.com>
2022-11-01 14:45:35 +1300

    Add basic tab view

---
+++ DotSyncIOS/DotSyncIOS/Views/SettingsView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsView.swift
+//  DotSyncIOS
+//
+//  Created by Fernando Zhu on 1/11/22.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text("Settings view")
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
